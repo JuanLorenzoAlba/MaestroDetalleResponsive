bac791ccefe202fed76a3432b527b6f01fc91c79
JuanLorenzoAlba <albajual@gmail.com>
2025-05-21 10:08:13 -0300

    first commit

---
+++ app/src/main/java/com/unaj/maestrodetalleresponsive/placeholder/PlaceholderContent.kt
@@ -0,0 +1,26 @@
+package com.unaj.maestrodetalleresponsive.placeholder
+
+object PlaceholderContent {
+
+    /**
+     * Lista de sitios web de ejemplo.
+     */
+    val ITEMS: List<PlaceholderItem> = listOf(
+        PlaceholderItem("1", "Amazon", "https://www.amazon.com"),
+        PlaceholderItem("2", "Google", "https://www.google.com"),
+        PlaceholderItem("3", "Android", "https://www.android.com"),
+        PlaceholderItem("4", "UNAJ", "https://www.unaj.edu.ar"),
+        PlaceholderItem("5", "Wikipedia", "https://www.wikipedia.org")
+    )
+
+    /**
+     * Un ítem que representa un sitio web.
+     */
+    data class PlaceholderItem(
+        val id: String,
+        val websiteName: String,
+        val websiteUrl: String
+    ) {
+        override fun toString(): String = websiteName
+    }
+}
